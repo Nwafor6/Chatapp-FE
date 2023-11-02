d9bbbdc3c1b34d6bea0862a310c554d10ac44874
nwafor6 <nwaforglory6@gmail.com>
2023-11-02 18:48:01 +0100

    added chatlist and chatscrool compoents for chats responsivesness

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,7 @@
 import React, {useState, useEffect} from "react"
 import NavBar from "./Nav"
+import { ChatList } from "./ChatList"
+import { ChatScroll } from "./ChatScroll"
 
 
 const Chat =()=>{
@@ -10,20 +12,8 @@ const Chat =()=>{
                 <div className="chat-main-body row mt-3">
                     <div className="chat-left-bar col-md-2 col-sm-12 shadow-sm">
                         <div className="title"><h4>Chats</h4></div><hr />
-                        <div className="chat-list">
-                            <div className="chat-user-name">
-                                <h6><i class="bi bi-person-circle"></i> Glory <i class="bi bi-dot"></i></h6>
-                                <div className="status"><small>Online</small></div>
-                            </div><hr />
-                            <div className="chat-user-name">
-                                <h6><i class="bi bi-person-circle"></i> Prince <i class="bi bi-dot"></i></h6>
-                                <div className="status"><small>Online</small></div>
-                            </div><hr />
-                            <div className="chat-user-name">
-                                <h6><i class="bi bi-person-circle"></i> David <i class="bi bi-dot"></i></h6>
-                                <div className="status"><small>Offline</small></div>
-                            </div><hr />
-                        </div>
+                        {<ChatList/>}
+                        {<ChatScroll/>}
                     </div>
                     <div className="chat-right-bar4-messaging col-md-10 col-sm-12 shadow-sm">
                         <div className="top-section mt-3"><i class="bi bi-person-circle"></i></div><hr />
---
+++ src/components/ChatScroll.jsx
@@ -0,0 +1,52 @@
+
+
+
+export const ChatScroll =()=>{
+    return (
+        <div className="chat-list table-responsive small-screen">
+            <table className="table">
+                <tbody>
+                    <tr>
+                    <td>
+                    <div className="chat-user-name">
+                        <h6><i class="bi bi-person-circle"></i> Glory <i class="bi bi-dot"></i></h6>
+                        <div className="status"><small>Online</small></div>
+                    </div>
+                    </td>
+                    <td>
+                    <div className="chat-user-name">
+                        <h6><i class="bi bi-person-circle"></i> Glory <i class="bi bi-dot"></i></h6>
+                        <div className="status"><small>Online</small></div>
+                    </div>
+                    </td>
+                    <td>
+                    <div className="chat-user-name">
+                        <h6><i class="bi bi-person-circle"></i> Glory <i class="bi bi-dot"></i></h6>
+                        <div className="status"><small>Online</small></div>
+                    </div>
+                    </td>
+                    <td>
+                    <div className="chat-user-name">
+                        <h6><i class="bi bi-person-circle"></i> Glory <i class="bi bi-dot"></i></h6>
+                        <div className="status"><small>Online</small></div>
+                    </div>
+                    </td>
+                    <td>
+                    <div className="chat-user-name">
+                        <h6><i class="bi bi-person-circle"></i> Glory <i class="bi bi-dot"></i></h6>
+                        <div className="status"><small>Online</small></div>
+                    </div>
+                    </td>
+                    <td>
+                    <div className="chat-user-name">
+                        <h6><i class="bi bi-person-circle"></i> Glory <i class="bi bi-dot"></i></h6>
+                        <div className="status"><small>Online</small></div>
+                    </div>
+                    </td>
+
+                    </tr>
+                </tbody>
+                </table>
+        </div>
+    )
+}
