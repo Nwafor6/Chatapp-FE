5df355c606dae583a70f4e7c1841ffe4fb379613
nwafor6 <nwaforglory6@gmail.com>
2023-11-05 05:01:45 +0100

    added socket production url

--- src/components/appConfig.jsx
+++ src/components/appConfig.jsx
@@ -1,3 +1,4 @@
 import io from 'socket.io-client'; 
-export const socket = io.connect('http://localhost:4000');
+// export const socket = io.connect('http://localhost:4000');
+export const socket = io.connect("https://chatz-socket.onrender.com")
 export const BASE_URL="http://localhost:8000"
